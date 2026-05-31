fa42fd0dddfc856584bf73df5fb79c27763ad889
Emmanuel Morgan <de.morganofficial@gmail.com>
2026-05-31 05:10:50 +0100

    Unit Tests

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
 def main():
-    x = int(input("What's x? "))
+    x = (input("What's x? "))
     print("x squared is", square(x))
 
 def square(n):
     return n ** 2
+
+
+if __name__ == "__main__":
+    main()
